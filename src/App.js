f20b6fb550ee2423c2811318de78c214eb7ae002
rajju <vajrachara@yahoo.com>
2019-11-21 10:08:38 +0000

    complete app refresed

--- src/App.js
+++ src/App.js
@@ -1,18 +1,11 @@
 import React from 'react';
-import Navbar from './components/Navbar';
-import BookList from './components/BookList';
-import ThemeContextProvider from './contexts/ThemeContext';
-import ToogleTheme from './components/ToogleTheme';
+
 
 
 function App() {
   return (
     <div className="App">
-      <ThemeContextProvider>
-          <Navbar />
-          <BookList />
-          <ToogleTheme />
-      </ThemeContextProvider>
+     <h1>H:ooks</h1>
     </div>
   );
 }
